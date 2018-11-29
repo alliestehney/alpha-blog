eed1b3da0c710a73c7a361bb480926ea628bc279
alliestehney <allie@quikly.com>
2018-11-28 22:16:23 -0500

    Restrict actions from UI based on permisison; update pages controller name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'page#home'
-  get '/about', to: 'page#about'
+  root 'pages#home'
+  get '/about', to: 'pages#about'
 
   # creates all the things for articles
   resources :articles
