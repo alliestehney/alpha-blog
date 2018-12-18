d4d918347c3dd22022259e191a6d7fc860481f64
alliestehney <allie@quikly.com>
2018-12-17 21:27:34 -0500

    Allow admin users to edit category names

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,9 +13,23 @@ class CategoriesController < ApplicationController
     @category = Category.new(category_params)
     if @category.save
       flash[:success] = "Category was created successfully"
-      redirect_to category_path
+      redirect_to category_path(@category)
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      flash[:success] = "#{@category.name} was successfully updated"
+      redirect_to category_path(@category)
+    else
+
     end
   end
 
